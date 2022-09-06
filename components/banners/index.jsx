5db9330e95c20c8f7ddbaac38fefcb9758f0e93b
Dercio <12DercioMaduna@gmail.com>
2022-09-06 09:41:23 +0200

    Preparing a dev link for my responsive testing.

--- components/banners/index.jsx
+++ components/banners/index.jsx
@@ -1,5 +1,17 @@
 import { Box, Typography, Avatar, Grid, Button, Fade, Stack } from "@mui/material"
-import { useState } from "react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade ,Pagination, Navigation } from "swiper";
+// import required modules
+// import { EffectFade, Navigation, Pagination } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 import { PulseLoader } from "react-spinners";
 import { Socials } from "../socials";
 
@@ -8,20 +20,20 @@ export const Banners = () => {
     const artists = [
         {
             name: "Dj Shadzo",
-            img: "",
-            embedItem: <iframe onLoad={()=>setLoading(false)} style={{ borderRadius: "12px", margin: '18px 0' }} src="https://open.spotify.com/embed/artist/5JuA3291INTaMk0R8xMkZK?utm_source=generator" width="100%" height="315" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>,
+            img: "/shadzo-2.jpeg",
+            embedItem: <iframe onLoad={() => setLoading(false)} style={{ borderRadius: "12px", margin: '18px 0' }} src="https://open.spotify.com/embed/artist/5JuA3291INTaMk0R8xMkZK?utm_source=generator" width="100%" height="315" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>,
             slogan: "If your girl dont know Shadzo don't dala"
         },
         {
             name: "YKM THEE MC",
-            img: "",
-            embedItem: <iframe onLoad={()=>setLoading(false)} style={{ borderRadius: "12px", margin: '18px 0' }} width="100%" height="315" src="https://www.youtube.com/embed/gy6gGT-G6ak" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen loading="lazy"></iframe>,
+            img: "/ykm-2.jpeg",
+            embedItem: <iframe onLoad={() => setLoading(false)} style={{ borderRadius: "12px", margin: '18px 0' }} width="100%" height="315" src="https://www.youtube.com/embed/gy6gGT-G6ak" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen loading="lazy"></iframe>,
             slogan: "Mr Lovers and Friends."
         },
         {
             name: "Karlo Dj",
-            img: "",
-            embedItem: <iframe onLoad={()=>setLoading(false)} style={{ borderRadius: "12px", margin: '18px 0' }} width="100%" height="315" src="https://www.youtube.com/embed/-J0fR8UMfIc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
+            img: "/Karlo-1.jpeg",
+            embedItem: <iframe onLoad={() => setLoading(false)} style={{ borderRadius: "12px", margin: '18px 0' }} width="100%" height="315" src="https://www.youtube.com/embed/-J0fR8UMfIc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
             slogan: "If your girl dont know Shadzo don't dala"
         }];
     const [active, setActive] = useState("");
@@ -32,7 +44,7 @@ export const Banners = () => {
             setActive(artist);
         } catch (error) {
             alert(error)
-        } 
+        }
         finally {
             setLoading(false);
         }
@@ -41,60 +53,111 @@ export const Banners = () => {
     return (
         <Box sx={{ marginTop: '4.5%', background: '#222' }}>
             <Grid container >
-                <Grid xs={12} md={5} sx={{ height: { lg:'91vh' , xs:'fit-content' } , padding: { lg: "52px 16px" }, background: '#111', display: 'flex', flexDirection: { xs:'row' , md:'column'} , alignItems: 'center', justifyContent: 'space-evenly' }}>
-                    <Typography variant="h2" width={"100%"} color={"#eee"} fontWeight={"600"} textAlign={"center"}>Meet The Artists</Typography>
-                    <Button onClick={()=> setActive("")} sx={{ fontWeight: '600', fontSize: '16px', padding: '32px 0', padding: '18px auto 0px auto', color:'#111' , background:'#eee' }}>All</Button>
-                    {artists.map((artist, index) => {
-                        return (
-                            <Button key={index} onClick={(e) => {
-                                e.preventDefault();
-                                handleArtistClick(artist);
-                            }} sx={{ width: { xs:"20%" , md:'70%' }, fontWeight: '600', fontSize: '16px', padding: '32px 0', margin: '18px auto 0px auto', background: '#eee', color: '#111', "&:hover": { color: '#eee' } }}>{artist.name}</Button>
-                        )
-                    })}
+                <Grid xs={12} md={5} sx={{ height: { lg: '91vh', xs: 'fit-content' }, padding: { lg: "52px 16px" }, background: '#111', display: 'flex', flexDirection: { xs: 'column', md: 'column' }, alignItems: 'center', justifyContent: 'space-evenly' }}>
+                    <Typography variant="h3" width={"100%"} color={"#eee"} fontWeight={"600"} textAlign={"center"} sx={{ marginTop: { xs: '50px' } }}>Meet The Artists</Typography>
+                    <Button onClick={() => setActive("")} sx={{ fontWeight: '600', fontSize: '16px', padding: '32px 0', padding: '18px auto 0px auto', margin: '18px auto', color: '#111', background: '#eee' }}>All</Button>
+
+                    <Grid container>
+
+
+                        {artists.map((artist, index) => {
+                            return (
+                                <Grid xs={4} md={12} sx={{ background: '', display: 'flex', alignItems: 'center' }}>
+                                    <Button key={index} onClick={(e) => {
+                                        e.preventDefault();
+                                        handleArtistClick(artist);
+                                    }} sx={{ width: { xs: "90%", md: '75%' }, fontWeight: active.name === artist.name ? "900" : '500', fontSize: '16px', padding: { xs: '16px 0', md: '32px 0' }, margin: '18px auto', background: '#eee', color: '#111', "&:hover": { color: '#eee' } }}>{artist.name}</Button>
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
                 </Grid>
-                <Grid xs={12} md={7} sx={{ height: { lg:'91vh' , xs:'fit-content' } , padding: { lg: "24px 16px" } }}>
+                <Grid xs={12} md={7} sx={{ height: { lg: 'fit-content', xs: 'fit-content' }, padding: { lg: "24px 16px" } , margin:'auto 0' }}>
                     {
                         active === "" ? (
-                            <Box sx={{ backgroundImage:'url("/misguided-2.jpeg")', backgroundSize:'contain', backgroundRepeat:'no-repeat' , backgroundPosition:'center' , height:'100%' }}>
+                            // <Box sx={{ backgroundImage: 'url("/misguided-2.jpeg")', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', margin:'auto 0' ,height: {lg:'500px' , xs:'250px'} }}>
 
-                            </Box>
+                            // </Box>
+                            <SquadSlides />
                         ) : (
                             <Fade in={active} sx={{ width: '100%', background: 'rgba(255,255,255,.1)', height: '100%', padding: { lg: "16px" } }} >
-                            <Box sx={{ padding: { lg: "2px 16px" } }}>
-                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-    
-                                    <Avatar src={active.img} alt={active.name} sx={{ width: { lg: '120px', xs: "38px" }, height: { lg: '120px', xs: "38px" } }} ></Avatar>
-                                    <Stack sx={{ margin: { lg: '0 21px' } }}>
-                                        <Typography variant="h6" color={"#eee"} fontWeight={"600"} textAlign={"start"}>{active.name}</Typography>
-                                        <Typography variant="p" color={"#eee"} fontWeight={"300"} textAlign={"center"}>{active.slogan}</Typography>
-    
-                                    </Stack>
+                                <Box sx={{ padding: { lg: "2px 16px" } }}>
+                                    <Box sx={{ display: 'flex', alignItems: 'center', padding: { xs: "16px 12px", md: "0px" } }}>
+
+                                        <Avatar src={active.img} alt={active.name} sx={{ width: { lg: '120px', xs: "80px" }, height: { lg: '120px', xs: "80px" } , filter:"blur(0.3px)" }} ></Avatar>
+                                        <Stack sx={{ margin: { xs: '0px 21px' } }}>
+                                            <Typography variant="h6" color={"#eee"} fontWeight={"600"} textAlign={"start"}>{active.name}</Typography>
+                                            <Typography variant="p" color={"#eee"} fontWeight={"300"} textAlign={"center"}>{active.slogan}</Typography>
+
+                                        </Stack>
+                                    </Box>
+
+                                    {
+                                        loading ? (
+                                            <Box sx={{ height: '350px', width: '100%', background: 'yellow', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                                <PulseLoader />
+                                            </Box>
+                                        )
+                                            : (
+                                                <Box sx={{ width: "100%", display: 'flex', alignItems: 'center', padding: { xs: "16px 12px", md: "0px" } }}>
+                                                    {
+                                                        active.embedItem
+                                                    }
+                                                </Box>
+                                            )
+                                    }
+                                    <Box sx={{
+                                        height: '48px',
+                                        background: { xs: 'rgba(255,255,255,.1)', md: 'transparent' },
+                                        display: 'flex',
+                                        justifyContent: 'flex-end'
+                                    }}>
+                                        <Socials artist={active} />
+                                    </Box>
                                 </Box>
-    
-                                {
-                                    loading ? (
-                                        <Box sx={{ height: '350px',width:'100%', background:'yellow' , display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                            <PulseLoader />
-                                        </Box>
-                                    )
-                                        :
-                                        active.embedItem
-                                }
-                                <Box sx={{
-                                    height: '48px',
-                                    background: "#111",
-                                    display:'flex',
-                                    justifyContent:'flex-end'
-                                }}>
-                                    <Socials artist={active}/>
-                                </Box>
-                            </Box>
-                        </Fade>
+                            </Fade>
                         )
                     }
                 </Grid>
             </Grid>
         </Box>
     )
+}
+
+export const SquadSlides = () => {
+    return (
+        <Box id="squad-slides">
+            <Swiper
+                spaceBetween={30}
+                effect={"fade"}
+                autoplay={{
+                    delay: 3500,
+                    disableOnInteraction: false,
+                }}
+                navigation={false}
+
+                modules={[EffectFade, Autoplay, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src="/misguided-2.jpeg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/misguided-3.jpeg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/misguided-4.jpeg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/misguided-5.jpeg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/misguided-6.jpeg" />
+                </SwiperSlide>
+
+
+
+            </Swiper>
+        </Box>
+    );
 }
--- components/events/index.jsx
+++ components/events/index.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Box, Typography } from "@mui/material"
+import { Box, Grid, Typography } from "@mui/material"
 
 // Import Swiper styles
 import "swiper/css";
@@ -15,36 +15,44 @@ import { EffectCoverflow, Pagination } from "swiper";
 
 export const Events = () => {
   return (
-    <Box sx={{ padding: '48px', height: '65vh', background: '#111', }}>
-      <Typography variant="h3" width={"100%"} color={"#eee"} fontWeight={"600"}>Events & Gigs</Typography>
+    <Box id="events" sx={{ padding: '21px 0', height: '65vh', background: '#111', width: '100%' }}>
 
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <Box sx={{ height: '300px', width: '300px', backgroundSize: "cover", backgroundImage: 'url("/run-the-city.jpeg")' }} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Box sx={{ height: '300px', width: '300px', backgroundSize: "cover", backgroundImage: 'url("/girls-do-it-better.jpeg")' }} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Box sx={{ height: '300px', width: '300px', backgroundSize: "cover", backgroundImage: 'url("/spring-fling.jpeg")' }} />
-        </SwiperSlide>
+      <Grid container>
+        <Grid xs={12} md={5} sx={{ padding: { xs: "12px 8px" } , textAlign:{ xs:"center" } , margin: "0px 0px 18px 0px", }} >
+          <Typography variant="p" width={"100%"} color={"#eee"} sx={{ fontSize: { xs: "32px", md: "54px", lg: "62px" } }} fontWeight={"600"}>Events & Gigs</Typography>
 
-      </Swiper>
+        </Grid>
+
+        <Grid xs={12} md={7} sx={{ display: 'flex', alignItems: 'center', background: '' }}>
+          <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            pagination={true}
+            modules={[EffectCoverflow, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <Box sx={{ height: '300px', width: '300px', backgroundSize: "cover", backgroundImage: 'url("/run-the-city.jpeg")' }} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Box sx={{ height: '300px', width: '300px', backgroundSize: "cover", backgroundImage: 'url("/girls-do-it-better.jpeg")' }} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Box sx={{ height: '300px', width: '300px', backgroundSize: "cover", backgroundImage: 'url("/spring-fling.jpeg")' }} />
+            </SwiperSlide>
+
+          </Swiper>
+        </Grid>
+      </Grid>
     </Box>
   );
 }

0aa92fb0f873cbf949276a6021cc39edee09765f
Dercio <12DercioMaduna@gmail.com>
2022-09-06 13:07:06 +0200

    Fixed the events section.

--- components/events/index.jsx
+++ components/events/index.jsx
@@ -19,12 +19,12 @@ export const Events = () => {
     <Box id="events" sx={{ padding: '21px 0', height: 'fit-content', background: '#111', width: '100%' }}>
 
       <Grid container>
-        <Grid item xs={12} md={5} sx={{ padding: { xs: "12px 8px" }, textAlign: { xs: "center" }, margin: "0px 0px 18px 0px", }} >
+        <Grid item xs={12} sx={{ padding: { xs: "12px 8px" }, textAlign: { xs: "center" }, margin: "0px 0px 18px 0px", }} >
           <Typography variant="p" width={"100%"} color={"#eee"} sx={{ fontSize: { xs: "32px", md: "54px", lg: "62px" } }} fontWeight={"600"}>Events & Gigs</Typography>
 
         </Grid>
 
-        <Grid item xs={12} md={7} sx={{ display: 'flex', alignItems: 'center', background: '' }}>
+        <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center', background: '' }}>
           <Swiper
             effect={"coverflow"}
             grabCursor={true}
@@ -37,7 +37,7 @@ export const Events = () => {
               delay: 2500,
               disableOnInteraction: false,
             }}
-            slidesPerView={"auto"}
+            slidesPerView={2}
             navigation={true}
             coverflowEffect={{
               rotate: 50,
